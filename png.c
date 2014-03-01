8688411d9f71dacbb3710ccedac0dffaaba26011
Dylan Harris <dharris93@gmail.com>
2014-03-01 13:45:37 -0800

    Now validates PNG file - checks first 8 bytes in file with PNG signature

--- png.c
+++ png.c
@@ -11,5 +11,16 @@
  */
 int analyze_png(FILE *f) {
     /* YOU WRITE THIS PART */
-    return -1;
+    /* 1. Parse Header to validate PNG file
+     */
+    //size_t crc = crc32(0L, Z_NULL, 0);
+    int png_signature[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+    char signature[8];
+    fread(signature, 8, 1, f);
+    for (int i = 0; i < 8; i++) {
+        if (signature[i] != png_signature[i]) {
+            return -1;
+        }
+    }
+    return 0;
 }
